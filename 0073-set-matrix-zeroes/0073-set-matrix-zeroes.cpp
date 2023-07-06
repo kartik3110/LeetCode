5e5797ecf29e4e5ebe4220d8069e86c5ec3365f1
Kartik Gupta <115152849+kartik3110@users.noreply.github.com>
2023-07-06 19:13:54 +0530

    Time: 24 ms (11.31%), Space: 13.2 MB (85.23%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,68 +2,6 @@
     public:
         void setZeroes(vector<vector<int>>& matrix) {
 
-            // int m = matrix[0].size();
-            // int n = matrix.size();
-            
-          
-            // int rowZero = 1;
-            // for(int i =0; i<n; i++)
-            // {
-            //     for(int j= 0; j<m; j++)
-            //     {
-            //         if(matrix[i][j] == 0)
-            //         {
-            //             // marking ith row
-            //             matrix[i][0] = 0;
-
-            //             //checking for jth column
-            //             if(j != 0)
-                        
-            //                 matrix[0][j] =0;
-                        
-            //             else
-                        
-            //                 rowZero = 0;
-                        
-            //         }
-            //     }
-            // }
-
-            //     for(int i = 1; i < n; i++)
-            //     {
-            //         for(int j = 1; j < m; j++)
-            //         {
-            //             if(matrix[i][j] != 0)
-            //             {
-            //                 //check for row as well as column
-            //                 if(matrix[i][0] == 0 || matrix[0][j] == 0)
-            //                 {
-            //                     matrix[i][j] = 0;
-            //                 }
-            //             }
-            //         }
-            //     } 
-                
-            //      if(matrix[0][0] == 0)
-            //     {
-            //         for(int j = 0; j < m; j++)
-            //         {
-            //             matrix[0][j] = 0;
-            //         }
-            //     }
-
-            //     if(rowZero == 0)
-            //     {
-            //         for(int i = 0; i < n; i++)
-            //         {
-            //             matrix[i][0] = 0;
-            //         }
-            //     }
-                
-//             }
-            
-
-
             int m = matrix.size();
             int n = matrix[0].size();
             int rowZero = 1;
