3970c0fd766e034c1cdf38eba65ecb659c1037ad
Kartik Gupta <115152849+kartik3110@users.noreply.github.com>
2023-07-07 13:17:42 +0530

    Time: 38 ms (82.96%), Space: 28.1 MB (71.41%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -2,28 +2,43 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums)
     {
-        map<int, int> sum;
-        int ans = INT_MAX;
-        int ans2 = INT_MAX-1;
-        int j =0;
-        sum[-1] = 0;
-        for(int i = 0; i < nums.size(); i++)
+        int n = nums.size();
+        int l =0, r = 0, sum = 0, len = INT_MAX;
+        while(r<n)
         {
-            sum[i] = sum[i-1] + nums[i];
-            if(sum[i] >= target)
+            sum += nums[r++];
+            while(sum >= target)
             {
-                ans2 = min(i+1, ans2+1);
-                while(sum[i] - nums[j] >= target)
-                {
-                    ans2--;
-                    sum[i] -= nums[j];
-                    j++;
-                }
-                ans = min(ans, ans2);
+                len = min(len, r-l);
+                sum -= nums[l++];
             }
         }
-        if(ans == INT_MAX)
-            return 0;
-        return ans; 
+        return (len == INT_MAX)? 0 : len;
+        
     }
 };
+
+// first try, ineffecient
+        // map<int, int> sum;
+        // int ans = INT_MAX;
+        // int ans2 = INT_MAX-1;
+        // int j =0;
+        // sum[-1] = 0;
+        // for(int i = 0; i < nums.size(); i++)
+        // {
+        //     sum[i] = sum[i-1] + nums[i];
+        //     if(sum[i] >= target)
+        //     {
+        //         ans2 = min(i+1, ans2+1);
+        //         while(sum[i] - nums[j] >= target)
+        //         {
+        //             ans2--;
+        //             sum[i] -= nums[j];
+        //             j++;
+        //         }
+        //         ans = min(ans, ans2);
+        //     }
+        // }
+        // if(ans == INT_MAX)
+        //     return 0;
+        // return ans; 
