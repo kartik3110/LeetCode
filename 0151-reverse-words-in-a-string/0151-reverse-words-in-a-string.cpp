4c55bab6376aa4aad20de5fa391a0a36aba05eee
Kartik Gupta <115152849+kartik3110@users.noreply.github.com>
2023-09-13 14:13:44 +0530

    Time: 3 ms (86.78%), Space: 20.9 MB (15.56%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,26 +1,60 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        stack<string> st;
-        for(int i = 0; i < s.length(); i++)
-        {
-            if(s[i] == ' ') continue;
-            string word = "";
-            while(s[i] != ' ' && i < s.length())
-            {
-                word += s[i];
-                i++;
-            }
-            st.push(word);
-        }
+    string reverseWords(string s)
+    {
+        int i = 0;
         string ans = "";
-        while(!st.empty())
+        string temp = "";
+        while(i < s.length())
         {
-            ans += st.top();
-            ans += ' ';
-            st.pop();
+            char c = s[i];
+            if(c != ' ')
+            {
+                temp+= c;
+            }
+            else if(c == ' ')
+            {
+                if(temp != "")
+                {
+                    //add in reverse manner only
+                    ans = temp +' '+ ans;
+                    temp = "";
+                }
+            }
+            i++;
         }
+        if(temp != "") ans = temp +' '+ans;
         return ans.substr(0, ans.length()-1);
-        
+
     }
+
 };
+
+
+
+
+
+
+    // O(N) time and space
+    // string reverseWords(string s) {
+    //     stack<string> st;
+    //     for(int i = 0; i < s.length(); i++)
+    //     {
+    //         if(s[i] == ' ') continue;
+    //         string word = "";
+    //         while(s[i] != ' ' && i < s.length())
+    //         {
+    //             word += s[i];
+    //             i++;
+    //         }
+    //         st.push(word);
+    //     }
+    //     string ans = "";
+    //     while(!st.empty())
+    //     {
+    //         ans += st.top();
+    //         ans += ' ';
+    //         st.pop();
+    //     }
+    //     return ans.substr(0, ans.length()-1);
+    // }
